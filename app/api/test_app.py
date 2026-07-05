a7f41f5fd4e7f8bace76dcf1764e75f3286523cf
Mauricio Amado <maoamado@Mauricios-MacBook-Air.local>
2026-07-05 11:33:02 -0500

    :rocket: Adding porpuse error to pipeline

--- app/api/test_app.py
+++ app/api/test_app.py
@@ -6,7 +6,7 @@ def test_health_endpoint():
     response = client.get("/health")
 
     assert response.status_code == 200
-    assert response.json["status"] == "ok"
+    assert response.json["status"] == "healthy" #assert response.json["status"] == "ok"
     assert response.json["service"] == "api"
 
 
